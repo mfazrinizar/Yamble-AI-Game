169c48a9e23a01da588327c343cb9bc5144fb62f
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-07-21 09:47:31 +0700

    Feat & Refactor: add ranks assets & change theme

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Yamble: Yap to Gamble AI Game',
       theme: theme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
