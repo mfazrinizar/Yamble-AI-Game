332e9197c01cc9cbcb26929e3ef1f5ee175269b7
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-07-23 10:44:02 +0700

    Fix: constraints/flex issues in LeaderboardPage

--- lib/pages/leaderboard/leaderboard_page.dart
+++ lib/pages/leaderboard/leaderboard_page.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:yamble_yap_to_gamble_ai_game/utils/hex_color.dart';
 
 class LeaderboardPage extends StatefulWidget {
   const LeaderboardPage({super.key});
@@ -66,6 +65,10 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
 
   @override
   Widget build(BuildContext context) {
+    final top = MediaQuery.of(context).padding.top;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
@@ -88,162 +91,200 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
               ),
         ),
       ),
-      body: FutureBuilder<List<DocumentSnapshot>>(
-        future: _fetchLeaderboardData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-              child: Text('No data found.'),
-            );
-          }
-
-          List<DocumentSnapshot> leaderboardData = snapshot.data!;
-
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(32.0),
-                topRight: Radius.circular(32.0),
+      body: Stack(
+        children: [
+          // Background SVG image
+          Positioned(
+            top: -top,
+            left: 0,
+            width: width,
+            height: height * 0.4,
+            child: SizedBox(
+              child: SvgPicture.asset(
+                'assets/images/leaderboard.svg',
               ),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: const Offset(0, 3),
-                ),
-              ],
             ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 15,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListView.builder(
-                      itemCount: leaderboardData.length,
-                      itemBuilder: (context, index) {
-                        var userDoc = leaderboardData[index];
+          ),
+          FutureBuilder<List<DocumentSnapshot>>(
+            future: _fetchLeaderboardData(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-                        var name = userDoc['name'];
-                        var wins = userDoc['totalWin'];
-                        var rank = userDoc['rank'];
-                        var uid = userDoc['uid'];
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(
+                  child: Text('No data found.'),
+                );
+              }
 
-                        return Container(
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8.0),
-                            color:
-                                Theme.of(context).primaryColor.withOpacity(0.2),
-                            // boxShadow: [
-                            //   BoxShadow(
-                            //     color: Colors.grey.withOpacity(0.5),
-                            //     spreadRadius: 2,
-                            //     blurRadius: 5,
-                            //     offset: const Offset(0, 3),
-                            //   ),
-                            // ],
-                          ),
-                          child: SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: [
-                                CircleAvatar(
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor,
-                                  child: Text(
-                                    (index + 1).toString(),
-                                    style: const TextStyle(color: Colors.white),
-                                  ),
+              List<DocumentSnapshot> leaderboardData = snapshot.data!;
+
+              return DraggableScrollableSheet(
+                initialChildSize: 0.6,
+                minChildSize: 0.6,
+                maxChildSize: 1.0,
+                builder: (context, scrollController) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(32.0),
+                        topRight: Radius.circular(32.0),
+                      ),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: CustomScrollView(
+                      controller: scrollController,
+                      slivers: [
+                        const SliverToBoxAdapter(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Center(
+                                child: CircleAvatar(
+                                  child: Icon(Icons.leaderboard),
                                 ),
-                                const SizedBox(width: 10),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    // Text(
-                                    //   name + (_currentUser.uid == uid) ? ' (You)' : '',
-                                    //   style: Theme.of(context)
-                                    //       .textTheme
-                                    //       .titleLarge,
-                                    // ),
-                                    RichText(
-                                      text: TextSpan(
-                                        text: name,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleLarge,
+                              ),
+                              SizedBox(
+                                height: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SliverList(
+                          delegate: SliverChildBuilderDelegate(
+                            (context, index) {
+                              var userDoc = leaderboardData[index];
+
+                              var name = userDoc['name'];
+                              var wins = userDoc['totalWin'];
+                              var rank = userDoc['rank'];
+                              var uid = userDoc['uid'];
+
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    color: Theme.of(context)
+                                        .primaryColor
+                                        .withOpacity(0.2),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      CircleAvatar(
+                                        backgroundColor:
+                                            Theme.of(context).primaryColor,
+                                        child: Text(
+                                          (index + 1).toString(),
+                                          style: const TextStyle(
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
                                         children: [
-                                          TextSpan(
-                                            text: (_currentUser.uid == uid)
-                                                ? ' (You)'
-                                                : '',
-                                            style: TextStyle(
-                                              color: Theme.of(context)
-                                                  .primaryColor,
-                                              fontWeight: FontWeight.bold,
+                                          ConstrainedBox(
+                                            constraints: BoxConstraints(
+                                              maxWidth: width * 0.4,
                                             ),
+                                            child: SingleChildScrollView(
+                                              scrollDirection: Axis.horizontal,
+                                              child: RichText(
+                                                text: TextSpan(
+                                                  text: name,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .titleLarge,
+                                                  children: [
+                                                    TextSpan(
+                                                      text: (_currentUser.uid ==
+                                                              uid)
+                                                          ? ' (You)'
+                                                          : '',
+                                                      style: TextStyle(
+                                                        color: Theme.of(context)
+                                                            .primaryColor,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          Text(
+                                            '$wins Wins',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleMedium,
                                           ),
                                         ],
                                       ),
-                                    ),
-                                    Text(
-                                      '$wins Wins',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleMedium,
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(width: 10),
-                                Container(
-                                  width: 2,
-                                  height: 40,
-                                  color: Colors.white,
-                                ),
-                                const SizedBox(width: 10),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    CircleAvatar(
-                                      backgroundColor: HexColor('#E5DEFF'),
-                                      child: SvgPicture.asset(
-                                        getRankAsset(rank),
+                                      const Spacer(),
+                                      Container(
+                                        width: 2,
+                                        height: 40,
+                                        color: Colors.white,
                                       ),
-                                    ),
-                                    const SizedBox(width: 10),
-                                    Text(
-                                      getRankTitle(rank),
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleLarge
-                                          ?.copyWith(
-                                              color: Theme.of(context)
-                                                  .primaryColor),
-                                    ),
-                                  ],
+                                      const Spacer(),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          CircleAvatar(
+                                            backgroundColor: Theme.of(context)
+                                                .primaryColor
+                                                .withOpacity(0.1),
+                                            child: SvgPicture.asset(
+                                              getRankAsset(rank),
+                                            ),
+                                          ),
+                                          const SizedBox(width: 10),
+                                          Text(
+                                            getRankTitle(rank),
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleLarge
+                                                ?.copyWith(
+                                                    color: Theme.of(context)
+                                                        .primaryColor),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ],
-                            ),
+                              );
+                            },
+                            childCount: leaderboardData.length,
                           ),
-                        );
-                      },
+                        ),
+                      ],
                     ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
